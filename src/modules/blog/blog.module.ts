259b799f20aaf64ad0b74d52c31d989d4b5be131
KenEzekiel <kenneth.ezekiel.suprantoni@xendit.co>
2025-12-26 13:33:46 +0700

    fix: Add missing PrismaModule and AuthModule imports to BlogModule

--- src/modules/blog/blog.module.ts
+++ src/modules/blog/blog.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { PackagesController, PostsController } from './blog.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 import { SupabaseModule } from 'src/services/supabase/supabase.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [PrismaModule, SupabaseModule, AuthModule],
   providers: [BlogService],
   controllers: [PackagesController, PostsController],
   exports: [BlogService],
